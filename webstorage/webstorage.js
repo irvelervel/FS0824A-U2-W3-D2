20e556b338b75205a9e04f864e9072bf9778da8c
Stefano Casasola <ste.casasola@gmail.com>
2024-11-12 09:47:53 +0100

    first commit

---
+++ webstorage/webstorage.js
@@ -0,0 +1,4 @@
+// Le WEB STORAGE API nascono con la standardizzazione di HTML5, al fine di migliorare una tecnologia esistente (cookies).
+// Li migliorano superando le loro 2 limitazioni principali:
+// 1) I cookies possono memorizzare al massimo 4KB di dati
+// 2) I cookies non erano stati pensati per il concetto di "sessione"
